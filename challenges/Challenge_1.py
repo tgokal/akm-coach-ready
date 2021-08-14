ba2f1d9335855bdfaf7babb610a8dd72d473215e
Thasmika Gokal <tgokal@microsoft.com>
2021-08-14 15:48:09 +1000

    add Challenge 1 functions

--- challenges/Challenge_1.py
+++ challenges/Challenge_1.py
@@ -4,7 +4,8 @@ import time
 from azure.cognitiveservices.knowledge.qnamaker.authoring import QnAMakerClient
 from azure.cognitiveservices.knowledge.qnamaker.runtime import QnAMakerRuntimeClient
 from msrest.authentication import CognitiveServicesCredentials
-from utils.kb_helper import create_kb, download_kb, publish_kb, getEndpointKeys_kb, generate_answer, delete_kb, _monitor_operation
+from utils.kb_helper import create_kb, download_kb, publish_kb, getEndpointKeys_kb
+from utils.kb_helper import generate_answer, _monitor_operation
 
 #Key and endpoint
 subscription_key = '<subscription key>'
@@ -14,13 +15,16 @@ authoring_endpoint = '<authoring endpoint from Azure>'
 runtime_endpoint = '<runtime endpoint from Azure>'
 
 #Instantiate client application
-client = QnAMakerClient(endpoint=authoring_endpoint, credentials=CognitiveServicesCredentials(subscription_key))
 
+client = QnAMakerClient(endpoint=authoring_endpoint, 
+                        credentials=CognitiveServicesCredentials(subscription_key))
 kb_id = create_kb(client=client)
 #TODO: Update KB function
 publish_kb(client=client, kb_id=kb_id)
 download_kb(client=client, kb_id=kb_id)
 queryRuntimeKey = getEndpointKeys_kb(client=client)
 #Authenticate runtime from client application
-runtimeClient = QnAMakerRuntimeClient(runtime_endpoint=runtime_endpoint, credentials=CognitiveServicesCredentials(queryRuntimeKey))
-generate_answer(client=runtimeClient,kb_id=kb_id,runtimeKey=queryRuntimeKey)
+runtimeClient = QnAMakerRuntimeClient(runtime_endpoint=runtime_endpoint, 
+                                      credentials=CognitiveServicesCredentials(queryRuntimeKey))
+generate_answer(client=runtimeClient,kb_id=kb_id,
+                runtimeKey=queryRuntimeKey)
